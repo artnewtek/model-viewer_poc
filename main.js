381e9e167cad0832a98792bcfadb91891ba39893
Artem Skitenko <artemskitenko93@gmail.com>
2021-11-19 01:41:28 -0600

    cleanup

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-window.onload = function() {
-  var viewer = document.getElementById("viewer");
-  viewer.style.height = screen.availHeight + "px";
-  viewer.style.width = screen.availWidth + "px";
-
-  console.log(viewer.getCameraOrbit())
-};
+var viewer = document.getElementById("viewer");
+console.log(viewer.getCameraOrbit());
